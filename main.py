e817152e77cc94b3611e46d54322e5b3d0b2b310
Thales Nathan <thcmatias@gmail.com>
2018-06-28 18:34:10 -0300

    Pequena CLI

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ matriz =[]
 matrizTreino = []
 listaPessoas = []
 
+print("Carregando áudios de famosos, aguarde...")
 for i in audios:
     nome_famoso = i.split('_')[0]
     Itens = Matriz.gerar_matriz("audio_famosos/" + i, nome_famoso)
@@ -16,20 +17,26 @@ for i in audios:
         matrizTreino.append(iten[1])
     #matriz.append(Matriz.gerar_matriz("audio_famosos/" + i, nome_famoso))
 
-audioUsuario = Microfone.audio_usuario()
+print("Pronto!")
+input("BEM VINDO(A)!\nPara começar aperte qualquer tecla e seu áudio será gravado.")
+user_answer = 'y'
 
-matrizUsuario = Matriz.gerar_matriz(audioUsuario, "Usuario")
+while user_answer == 'y':
+    print("GRAVANDOOOO! Imite alguma celebridade, rápido!")
+    audioUsuario = Microfone.audio_usuario()
+    print("OK, vou adivinhar quem você imitou agora...")
+    matrizUsuario = Matriz.gerar_matriz(audioUsuario, "Usuario")
 
+    #print(listaPessoas)
+    #print(matrizTreino)
 
+    clf = svm.SVC()
+    clf.fit(matrizTreino, listaPessoas)
 
-#print(listaPessoas)
-#print(matrizTreino)
+    result = clf.predict(matrizUsuario[0][1])
+    print("E o resultado foi...")
+    print(result, "!!!")
+    user_answer = str(input("Deseja imitar de novo? (digite 'y' para sim)")).lower
 
-clf = svm.SVC()
-clf.fit(matrizTreino, listaPessoas)
-
-result = clf.predict(matrizUsuario[0][1])
-print("Acabou")
-
-#
-
+print("OK! Foi divertido jogar com você! Até a próxima!")
+input("Fim do programa, aprte qualquer tecla para finalizar.")
